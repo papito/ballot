23c9b9ce64b910776de29345fbecbbf70b8f88ea
Andrei Taranchenko <drey10@gmail.com>
2019-09-22 11:18:19 -0400

    Landing screen done.

--- ui/src/typescript/components/Landing.vue
+++ ui/src/typescript/components/Landing.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="landing">
     <div class="row">
-      <div class="col centered">
-        <router-link class="nav-link" to="/new">New Voting Space</router-link>
+      <div class="col">
+        <router-link class="nav-link" to="/new">
+          <button type="button" class="btn btn-lg btn-warning">New Voting Space</button>
+        </router-link>
       </div>
     </div>
   </div>
