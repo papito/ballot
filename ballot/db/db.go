52256730f76ae06c74fa5d7bbb2829ec7132d135
Andrei Taranchenko <papito@users.noreply.github.com>
2023-12-24 10:19:08 -0500

    Bringing IdleTimeout down AGAIN.

--- ballot/db/db.go
+++ ballot/db/db.go
@@ -62,7 +62,7 @@ var Const = struct {
 func newPool(server string) *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:     3,
-		IdleTimeout: 60 * time.Second,
+		IdleTimeout: 30 * time.Second,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.DialURL(server)
 			if err != nil {
