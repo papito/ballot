01c600d3569e5d21e26d8e5c8909e4bee2e66751
Andrei Taranchenko <drey10@gmail.com>
2024-10-15 10:56:21 -0400

    Request to new session but I have no idea how to use React state yet.

--- ballot-ui/src/views/Landing.tsx
+++ ballot-ui/src/views/Landing.tsx
@@ -1,34 +1,43 @@
 import './Landing.css'
 import '../components/Brand.tsx'
+import {useState} from 'react';
 import Brand from '../components/Brand.tsx';
 import Footer from '../components/Footer.tsx';
+import axios from 'axios';
 
-function MainContent() {
-    return (
-        <div id="Landing">
-            <Brand/>
-
-            <div className="form">
-                <form>
-                    <label htmlFor=""></label>
-                    <input type="text"
-                           maxLength={64}
-                           placeholder="Your name/alias"/>
-                    <button type="button">New Voting Space
-                    </button>
-                </form>
-            </div>
-
-            <Footer/>
-        </div>
-    )
-}
 
 function Landing() {
+    let [sessionId, setSessionId] = useState('')
+
+    function createNewSession() {
+        axios
+            .post("/api/session")
+            .then((response) => {
+                setSessionId(response.data.id)
+                console.log("session id: " + sessionId)
+            })
+            .catch((error) => console.error(error))
+    }
+
     return (
-        <div>
-            <MainContent/>
-        </div>
+            <div id="Landing">
+                <Brand/>
+
+                <div className="form">
+                    <form>
+                        <label htmlFor=""></label>
+                        <input type="text"
+                               maxLength={64}
+                               placeholder="Your name/alias"/>
+                        <button
+                            type="button"
+                        onClick={createNewSession}>New Voting Space
+                        </button>
+                    </form>
+                </div>
+
+                <Footer/>
+            </div>
     )
 }
 
