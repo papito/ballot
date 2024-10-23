11bd819079fc5d9e96639a82677c40519d90da82
Andrei Taranchenko <drey10@gmail.com>
2024-10-23 10:51:20 -0400

    Working JSX and Jest test, but still have an import warning in IntelliJ

---
+++ ballot-ui/tests/landing.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+
+import '@testing-library/jest-dom'
+import Landing from '../src/views/landing.tsx'
+
+describe('Login component tests', () => {
+    beforeEach(() => {})
+
+    afterEach(() => {})
+
+    it('Landing page renders correctly', async () => {
+        render(<Landing />)
+        const newVotingSpaceBtnTxt = screen.getByText('New Voting Space')
+        expect(newVotingSpaceBtnTxt).toBeInTheDocument()
+    })
+})
