a38f88617d5a1128cf0c5782d056326c6772bb92
Andrei Taranchenko <drey10@gmail.com>
2024-10-31 06:59:35 -0400

    Adding flashing prompt.

--- ballot-ui/src/views/vote.tsx
+++ ballot-ui/src/views/vote.tsx
@@ -285,6 +285,9 @@ function Vote(): React.JSX.Element {
             <></>
         )
 
+    const promptJsx: React.JSX.Element =
+        session.status == SessionState.VOTING ? <span>Pick a card!</span> : <></>
+
     return (
         <div id="Vote" className="view">
             <Brand />
@@ -292,6 +295,9 @@ function Vote(): React.JSX.Element {
             <div id="voteContainer">
                 <div id="voteHeader">
                     <StartStop session={session} user={user} />
+                    <div id="prompt">
+                        {promptJsx}
+                    </div>
                     <div id="copySessionUrl">
                         <button className="btn copy-url">
                             <i className="fas fa-clipboard"></i>Copy session URL
